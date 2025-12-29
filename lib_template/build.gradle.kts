1fdf41a3876e1e973eb4aaf61c373ccc4be1c370
journe <journesky@gmail.com>
2025-12-30 04:43:40 +0800

    update versions.toml

--- lib_template/build.gradle.kts
+++ lib_template/build.gradle.kts
@@ -47,39 +47,38 @@ dependencies {
 	implementation(libs.material)
 	implementation(libs.androidx.activity)
 
-
-	implementation(libs.androidx.navigation.fragment.ktx)
-	implementation(libs.androidx.navigation.ui.ktx)
-	implementation(libs.androidx.lifecycle.livedata.ktx)
-	implementation(libs.androidx.lifecycle.viewmodel.ktx)
-
+//	implementation(libs.androidx.navigation.fragment.ktx)
+//	implementation(libs.androidx.navigation.ui.ktx)
+	// 方式2：引用bundles（批量引入，简化代码）
+	implementation(libs.bundles.navigation)
 
 	implementation(libs.hilt)
 	ksp(libs.hilt.compiler)
 
+	implementation(libs.retrofit)
+	implementation(libs.retrofit.gson)
+	implementation(libs.retrofit.logging)
 
-	implementation("com.squareup.retrofit2:retrofit:2.9.0")
-	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-	implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-
-	implementation("io.coil-kt:coil:2.7.0")
+	implementation(libs.coil)
 
 	implementation("com.github.journe:Android-logger:v2.2.1")
 
 //    implementation("com.guolindev.permissionx:permissionx:1.7.1")
 //	debugImplementation("com.guolindev.glance:glance:1.1.0")
 
-	implementation("com.github.li-xiaojun:XPopup:2.10.0")
-	implementation("com.blankj:utilcodex:1.31.1")
+	implementation(libs.xpopup)
+	implementation(libs.utilcodex)
 
-	implementation("io.github.jeremyliao:live-event-bus-x:1.8.0")
+	implementation(libs.eventbus)
 //    implementation("com.github.neo-turak:LiveEventBus:1.8.1")
 
-	debugImplementation("cat.ereza:customactivityoncrash:2.4.0")
-	implementation("com.github.getActivity:ShapeView:9.2")
+	debugImplementation(libs.crashview)
 
-	implementation("com.github.angcyo.DslTablayout:TabLayout:3.7.2")
-	implementation("com.github.angcyo.DslTablayout:ViewPager1Delegate:3.7.2")
-	implementation("com.github.angcyo.DslTablayout:ViewPager2Delegate:3.7.2")
+	implementation(libs.shapeview)
+
+	implementation(libs.dslTablayout)
+	implementation(libs.dslTablayout.vp1)
+	implementation(libs.dslTablayout.vp2)
+
 
 }
